7bea80302ca688d32adfe6f7278d9fa41ef318f9
BeingMohdAli <mohdalisaad868@gmail.com>
2026-07-01 11:49:32 +0530

    MistarlAI

--- 2_sequential_chain.py
+++ 2_sequential_chain.py
@@ -2,6 +2,8 @@ from langchain_mistralai import ChatMistralAI
 from dotenv import load_dotenv
 from langchain_core.prompts import PromptTemplate
 from langchain_core.output_parsers import StrOutputParser
+import os 
+os.environ["LANGCHAIN_PROJECT"] = "Sequential App"
 
 load_dotenv()
 
@@ -15,14 +17,23 @@ prompt2 = PromptTemplate(
     input_variables=['text']
 )
 
-model = ChatMistralAI(
+model1 = ChatMistralAI(
+    model="ministral-3b-latest",
+    temperature=0.7
+)
+model2 = ChatMistralAI(
     model="ministral-3b-latest",
     temperature=0.7
 )
 
 parser = StrOutputParser()
 
-chain = prompt1 | model | parser | prompt2 | model | parser
+chain = prompt1 | model1 | parser | prompt2 | model2 | parser
+
+config = {
+    'tags':['llm', 'report generation', 'summarization'],
+    'metadata': {'model1':'mistral','model2' : 'mistral'}
+}
 
 result = chain.invoke({'topic': 'Unemployment in India'})
 
